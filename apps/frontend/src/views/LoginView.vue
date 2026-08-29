2dd8adc7013e3748219ef49db061ad19a2a867da
Adam Griger <adaaamgriger@gmail.com>
2026-08-29 11:02:57 +0200

    Replace Vue starter with login view and add Tailwind.

---
+++ apps/frontend/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+const login = () => {
+  console.log(email.value, password.value)
+}
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <button type="submit" @click="login">Login</button>
+  </div>
+</template>
+
+<style scoped></style>
